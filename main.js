3a14a6b3323416c89f92a31e441a940e66c46588
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-09-14 21:53:13 +0700

    add hero-section

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("ri-close-line");
   navMenu.classList.toggle("swipe-left");
 });
+
+// swiperjs
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
