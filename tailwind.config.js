4d8ae95ca3fe798c3d0a8b42d9c53ece2fc07b43
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-09-20 21:42:50 +0700

    add instagram section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     fontFamily: {
       montserrat: ["montserrat", "sans-serif"],
